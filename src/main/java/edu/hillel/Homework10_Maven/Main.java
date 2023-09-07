1c31552361595b82d5903eb71f043132ed4b73c2
Oleksandr Kuznietsov <oleksandr.kuznietsov.1@gmail.com>
2023-09-07 17:39:26 +0300

    homework10

---
+++ src/main/java/edu/hillel/Homework10_Maven/Main.java
@@ -0,0 +1,15 @@
+package edu.hillel.Homework10_Maven;
+
+import org.example.App;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+        System.out.println(App.add(a, b));
+        System.out.println(App.subtract(a, b));
+        System.out.println(App.multiply(a, b));
+        System.out.println(App.divide(a, b));
+
+    }
+}
