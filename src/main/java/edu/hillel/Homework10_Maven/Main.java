cc8ab6957ec5e795a6fdf37621eadc337750edaa
Oleksandr Kuznietsov <oleksandr.kuznietsov.1@gmail.com>
2023-09-07 17:45:28 +0300

    homework10

--- src/main/java/edu/hillel/Homework10_Maven/Main.java
+++ src/main/java/edu/hillel/Homework10_Maven/Main.java
@@ -4,12 +4,13 @@ import org.example.App;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 4;
-        System.out.println(App.add(a, b));
-        System.out.println(App.subtract(a, b));
-        System.out.println(App.multiply(a, b));
-        System.out.println(App.divide(a, b));
+            int a = 8;
+            int b = 4;
+            System.out.println(App.add(a, b));
+            System.out.println(App.subtract(a, b));
+            System.out.println(App.multiply(a, b));
+            System.out.println(App.divide(a, b));
 
+        }
     }
-}
+
